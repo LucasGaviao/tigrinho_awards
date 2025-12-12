a92f3c5ca9786a34e2eb7f022378a38fda90064e
gabriel <gabrielbaeta@id.uff.br>
2025-12-11 22:19:39 -0300

    new: insertGame and getGames (all)

---
+++ lib/repositories/game_repository.dart
@@ -0,0 +1,28 @@
+import 'package:sqflite/sqflite.dart';
+import '../models/game.dart';
+import '../database/db_helper.dart';
+
+class GameRepository {
+  
+  Future<int> insertGame(Game game) async {
+    
+    final db = await DBHelper().database;
+
+    return await db.insert(
+      'game',
+      game.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace
+    );
+
+  }
+
+  Future<List<Game>> getGames() async {
+
+    final db = await DBHelper().database;
+
+    final List<Map<String, dynamic>> maps = await db.query('game');
+
+    return maps.map((mapa) => Game.fromMap(mapa)).toList();
+  }
+
+}
