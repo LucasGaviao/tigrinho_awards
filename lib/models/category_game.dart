171426a1ef4d7e2f78e8b9e36f4c1aba449a5522
lexguimaraes <alexfsg@id.uff.br>
2025-12-11 23:18:07 -0300

    add: Category_Game repo

--- lib/models/category_game.dart
+++ lib/models/category_game.dart
@@ -8,5 +8,24 @@ class CategoryGame {
     required this.categoryId,
     required this.gameId
   });
+
+
+
+  
+  Map<String, dynamic> toMap(){
+    return {
+      'id': id,
+      'category_id': categoryId,
+      'game_id': gameId
+    };
+  }
+
+  factory CategoryGame.fromMap(Map<String, dynamic> map) {
+    return CategoryGame (
+      id: map['id'],
+      categoryId: map['category_id'],
+      gameId: map['game_id'],
+    );
+  }
   
 }
