5bebd9416edd7e5046c72e022bf52aa214db5f18
Gabriel Baeta <gabrielbaeta@id.uff.br>
2025-12-15 22:07:10 -0300

    feat: CRUD

--- lib/repositories/category_repository.dart
+++ lib/repositories/category_repository.dart
@@ -4,6 +4,7 @@ import '../database/db_helper.dart';
 
 class CategoryRepository {
   
+  //CREATE
   Future<int> insertCategory(Category category) async {
     
     final db = await DBHelper().database;
@@ -15,13 +16,57 @@ class CategoryRepository {
     );
   }
 
-  Future<List<Category>> getCategories() async {
+  //READ
+  Future<List<Category>> getAllCategories() async {
 
     final db = await DBHelper().database;
 
-    final List<Map<String, dynamic>> maps = await db.query('category_game');
+    final List<Map<String, dynamic>> maps = await db.query('category');
 
     return maps.map((mapa) => Category.fromMap(mapa)).toList();
+  }
+
+  Future<List<Category>> searchCategories(String name) async {
+
+    final db = await DBHelper().database;
+
+    final List<Map<String, dynamic>> maps = await db.query(
+      'category',
+      where: 'title LIKE ?',
+      whereArgs: ['%$name%'],
+    );
+
+    return maps.map((mapa) => Category.fromMap(mapa)).toList();
+  }
+
+  //UPDATE
+  Future<int> updateCategory(Category category) async {
+
+    final db = await DBHelper().database;
+
+    if(category.id == null){
+      print('Categoria sem ID');
+      return 0;
+    }
+
+    return await db.update(
+      'category',
+      category.toMap(),
+      where: 'id = ?',
+      whereArgs: [category.id],
+    );
+  }
+
+  //DELETE
+  Future<int> deleteCategory(int id) async {
+
+    final db = await DBHelper().database;
+
+    return await db.delete(
+      'category',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
   }
 
 }
