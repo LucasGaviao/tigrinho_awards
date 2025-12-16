a9b7fdb150f46c5df1ac3170d17bdbb86c75ffe0
Gabriel Baeta <gabrielbaeta@id.uff.br>
2025-12-15 22:55:59 -0300

    feat: CRD

--- lib/repositories/category_game_repository.dart
+++ lib/repositories/category_game_repository.dart
@@ -1,9 +1,11 @@
 import 'package:sqflite/sqflite.dart';
 import '../models/category_game.dart';
 import '../database/db_helper.dart';
+import '../models/game.dart';
 
 class CategoryGameRepository {
   
+  //CREATE
   Future<int> insertCatGame(CategoryGame categoryGame ) async {
     
     final db = await DBHelper().database;
@@ -15,6 +17,7 @@ class CategoryGameRepository {
     );
   }
 
+  //READ
   Future<List<CategoryGame>> getCatGames() async {
 
     final db = await DBHelper().database;
@@ -22,6 +25,46 @@ class CategoryGameRepository {
     final List<Map<String, dynamic>> maps = await db.query('category_game');
 
     return maps.map((mapa) => CategoryGame.fromMap(mapa)).toList();
+  }
+
+  Future<List<Game>> searchCatGames(String categoryName) async {
+
+    final db = await DBHelper().database;
+
+    final List<Map<String, dynamic>> maps = await db.rawQuery('''
+      SELECT game.* FROM game
+      INNER JOIN category_game on game.id = category_game.game_id
+      INNER JOIN category on category_game.category_id = category.id
+      WHERE category.title LIKE ?
+    ''', ['%$categoryName%']);
+  
+    return maps.map((mapa) => Game.fromMap(mapa)).toList();
+
+  }
+
+  //DELETE
+  Future<int> deleteCatGame(int id) async {
+
+    final db = await DBHelper().database;
+
+    return await db.delete(
+      'category_game',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+  }
+
+  Future<int> deleteAllCatGameHavingGameId(int gameId) async {
+
+    final db = await DBHelper().database;
+
+    return await db.delete(
+      'category_game',
+      where: 'gameId = ?',
+      whereArgs: [gameId],
+    );
+
   }
 
 }
