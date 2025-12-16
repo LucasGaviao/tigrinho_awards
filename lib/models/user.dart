ff8bd9e34dc80ff01969b1c6aecb89099e98e331
Gabriel Baeta <gabrielbaeta@id.uff.br>
2025-12-15 23:14:24 -0300

    feat: toMap and fromMap

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -13,4 +13,24 @@ class User{
     required this.role
   });
 
+  Map<String, dynamic> toMap(){
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'password': password,
+      'role': role
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      id: map['id'],
+      name: map['name'],
+      email: map['email'],
+      password: map['password'],
+      role: map['role'],
+    );
+  }
+
 }
